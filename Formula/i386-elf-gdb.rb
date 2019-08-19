c5329c57d0a12fa3eef74b5073945b8e72f0a9d5
unrevre <unrevre@gmail.com>
2019-08-18 22:47:37 -0400

    i386-elf-gdb: update gdb to 8.3

--- Formula/i386-elf-gdb.rb
+++ Formula/i386-elf-gdb.rb
@@ -1,9 +1,9 @@
 class I386ElfGdb < Formula
   desc "GNU debugger"
   homepage "https://www.gnu.org/software/gdb/"
-  url "https://ftp.gnu.org/gnu/gdb/gdb-8.2.1.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gdb/gdb-8.2.1.tar.xz"
-  sha256 "0a6a432907a03c5c8eaad3c3cffd50c00a40c3a5e3c4039440624bae703f2202"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-8.3.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-8.3.tar.xz"
+  sha256 "802f7ee309dcc547d65a68d61ebd6526762d26c3051f52caebe2189ac1ffd72e"
   head "https://sourceware.org/git/binutils-gdb.git"
 
   depends_on "pkg-config" => :build
