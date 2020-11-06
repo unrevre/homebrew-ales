fa5dbc207f6dfadc1d3fe9b750de0efff9d759d9
unrevre <unrevre@gmail.com>
2020-11-05 22:56:12 -0700

    i386-elf-gdb: update gdb to 10.1

--- Formula/i386-elf-gdb.rb
+++ Formula/i386-elf-gdb.rb
@@ -1,9 +1,9 @@
 class I386ElfGdb < Formula
   desc "GNU debugger"
   homepage "https://www.gnu.org/software/gdb/"
-  url "https://ftp.gnu.org/gnu/gdb/gdb-8.3.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gdb/gdb-8.3.tar.xz"
-  sha256 "802f7ee309dcc547d65a68d61ebd6526762d26c3051f52caebe2189ac1ffd72e"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-10.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-10.1.tar.xz"
+  sha256 "f82f1eceeec14a3afa2de8d9b0d3c91d5a3820e23e0a01bbb70ef9f0276b62c0"
   head "https://sourceware.org/git/binutils-gdb.git"
 
   depends_on "pkg-config" => :build
