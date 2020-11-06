cb9b48ae591214cb3cff363f532c0249106bc1b4
unrevre <unrevre@gmail.com>
2020-11-05 22:49:12 -0700

    i386-elf-binutils: update binutils to 2.35.1

--- Formula/i386-elf-binutils.rb
+++ Formula/i386-elf-binutils.rb
@@ -1,9 +1,9 @@
 class I386ElfBinutils < Formula
   desc "GNU binary tools for native development, targeting i386-elf"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.gz"
-  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.32.tar.gz"
-  sha256 "9b0d97b3d30df184d302bced12f976aa1e5fbf4b0be696cdebc6cca30411a46e"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.35.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.35.1.tar.gz"
+  sha256 "a8dfaae8cbbbc260fc1737a326adca97b5d4f3c95a82f0af1f7455ed1da5e77b"
 
   def install
     target = "i386-elf"
